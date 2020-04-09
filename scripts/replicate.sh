5416ef1657334cb3859706a9af72dc7ff72a1177
o.krauss <oliver.krauss@fh-hagenberg.at>
2020-04-09 12:40:28 +0200

    moved the file-rewriting to unpack

--- scripts/replicate.sh
+++ scripts/replicate.sh
@@ -4,17 +4,8 @@ cd "$(dirname "$0")"
 # clear the previous results if they existed
 rm -rf ../replication_results
 
-# move build bat to correct directory as csh does not understand working directories
-cp ./build.bat ../execution/gi_cbrt/cbrt/
-cd ../execution/gi_cbrt/cbrt/
-
-# edit the location of glibc in the compile.bat
-sed -i 's/\/tmp\/glibc-2.27/..\/..\/glibc-2.29/' compile.bat
-sed -i 's/main/.\/main/' fuzz*.bat
-sed -i 's/main/.\/main/' test*.bat
-
 # build the project
-#csh build.bat
+csh build.bat
 
 # create the replication result folder AFTER SUCCESS
 mkdir ../../../replication_results
--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -13,9 +13,22 @@ mv ./gi_cbrt ../execution
 gunzip -c ../dependencies/glibc-2.29.tar.gz | tar xvf -
 mv ./glibc-2.29 ../execution
 
+# move build bat to correct directory as csh does not understand working directories
+cp ./build.bat ../execution/gi_cbrt/cbrt/
+cd ../execution/gi_cbrt/cbrt/
+
+# edit the location of glibc in the compile.bat
+sed -i 's/\/tmp\/glibc-2.27/..\/..\/glibc-2.29/' compile.bat
+
+# edit main to include ./ as bat otherwise can't find them
+sed -i 's/main/.\/main/' fuzz*.bat
+sed -i 's/main/.\/main/' test*.bat
+
 # build glibc
-cd ../execution/glibc-2.29
+cd ../../glibc-2.29
 mkdir -p test/build
 cd test/build
 ../../configure --prefix=$build
 make
+
+
